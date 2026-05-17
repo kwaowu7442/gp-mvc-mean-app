84ea065893945c2e0d8fd7d640eae8c9472ed650
kwaowu7442 <kwaowu7442@students.ecpi.edu>
2026-05-17 19:43:10 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// if process.env.NODE_ENV is set to production, it reads from the production.js file;
+// if set to development it reads from the development.js file
+module.exports = require('./env/' + process.env.NODE_ENV);
